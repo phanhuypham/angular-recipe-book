43eb11628901cc17eacbd3e52d50a16673f96206
Phan Pham <phan_pham@mckinsey.com>
2023-10-01 17:56:21 +0700

    layout

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test recipe',
+      'this is a test',
+      'https://file.hstatic.net/200000303304/article/steak_10_b1b1397477ea4c8ca1f215989632a614_1024x1024.jpg'
+    ),
+    new Recipe(
+      'A Test recipe',
+      'this is a test',
+      'https://file.hstatic.net/200000303304/article/steak_10_b1b1397477ea4c8ca1f215989632a614_1024x1024.jpg'
+    ),
+  ];
+}
